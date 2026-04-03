a92b9b5b643ccd2696b75d58f7b0da5dba84851f
tejwant <tejwant2444@yopmail.com>
2026-04-03 12:36:46 +0530

    routes setting in main app file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
-import { useRoutes } from "react-router-dom";
-import { publicRoutes } from "./public";
+import { useRoutes } from 'react-router-dom';
+import { publicRoutes } from './public';
 
 export const AppRoutes: React.FC = () => {
-  // const navigate = useNavigate();
   const routes = useRoutes(publicRoutes);
   return routes;
 };
--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,16 +1,8 @@
-import { lazyImport } from "../lib/lazyImport";
-const AuthRoutes = lazyImport(
-  () => import("../features/auth/routes"),
-  "AuthRoutes",
-);
-
-// const Home = lazyImport(() => import("../features/home"), "Home");
-// const About = lazyImport(() => import("../features/about"), "About");
-// const Contact = lazyImport(() => import("../features/contact"), "Contact");
+import { lazyImport } from '../lib/lazyImport';
+const AuthRoutes = lazyImport(() => import('../features/auth/routes'), 'AuthRoutes');
+const CommonRoutes = lazyImport(() => import('../features/common/routes'), 'CommonRoutes');
 
 export const publicRoutes = [
-  //   { path: "/", element: <Home /> },
-  //   { path: "/about", element: <About /> },
-  //   { path: "/contact", element: <Contact /> },
-  { path: "/auth/*", element: <AuthRoutes /> },
+  { path: '/*', element: <CommonRoutes /> },
+  { path: '/auth/*', element: <AuthRoutes /> },
 ];
