d42d2b24040e0c9a3e0d2c8ca5d1f589fc56065f
tejwant <tejwant2444@yopmail.com>
2026-04-03 20:42:32 +0530

    token checker

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,21 @@
 import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import { publicRoutes } from './public';
+import { getProtectedRoutes } from './protectedRoutes';
+import { useUser } from '../lib/auth';
+import storage from '../lib/storage';
 
 export const AppRoutes: React.FC = () => {
-  const routes = useRoutes(publicRoutes);
-  console.log('routes', routes);
+  const { data: user, isLoading } = useUser();
+  const token = storage.getToken();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log('User:', user);
+  console.log('token', token);
+  const publicRouteElements = !token && user ? getProtectedRoutes(user?.role) : publicRoutes;
+  const routes = useRoutes([...publicRouteElements]);
+
   return <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>;
 };
